8e927bc16a4aedeed23aa615d6e1fe16ebf5a901
Victor Ferraz <victorfsf.dev@gmail.com>
2017-07-18 09:53:05 -0300

    [master] Update settings/prod.py

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -121,6 +121,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATE_FORMAT = 'd/m/Y'
+
+DATETIME_FORMAT = 'd-m-Y H:i:S'
+
+DECIMAL_SEPARATOR = ','
+
+USE_THOUSAND_SEPARATOR = True
+
+THOUSAND_SEPARATOR = '.'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
